75a8cf6caae970f9c9726377d96dd19606d448d5
Nicholas Sherlock <n.sherlock@gmail.com>
2019-04-08 08:43:57 +1200

    Bump version number in About dialog

--- js/gui/CPAboutDialog.js
+++ js/gui/CPAboutDialog.js
@@ -29,7 +29,7 @@ export default function CPAboutDialog(parent) {
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">About ChickenPaint</h5>
+                            <h5 class="modal-title">About ChickenPaint v2</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
